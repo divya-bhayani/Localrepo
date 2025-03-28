8f2b44110a868a6df3bad6960e9f9b851653efec
divya-bhayani <divyabhayani20@gmail.com>
2025-03-28 11:07:24 +0530

    forget changes

--- App.js
+++ App.js
@@ -2,5 +2,5 @@
   <p>helllo</p>
   <h1>branch featuare1</h1>
   <h1>fetuare1 chenged 2 tag not working(feature branch change)</h1>
-  <h1>fetuare1 chenged 2 tag not working (main file line code changed)</h1>
+  <h1>fetuare1 chenged 2 tag not working (main file line changed)</h1>
 </>;
